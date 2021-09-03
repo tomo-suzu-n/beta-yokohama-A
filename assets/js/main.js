d26fb418da1922e10453f98bc229c3ce3c9c3197
tomo-suzu-n <89887013+tomo-suzu-n@users.noreply.github.com>
2021-09-03 10:27:44 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,15 +18,15 @@ const app = Vue.createApp({
       */
       correctAnswer: {
         stage1: {
-          q1: 'あああ',
+          q1: 'AAA',
         },
         stage2: {
-          q1: 'いいい',
+          q1: 'BBB',
           // q2: 'えええ',
           // q3: 'おおお'
         },
         stage3: {
-          q1: 'ううう',
+          q1: 'CCC',
           // q2: 'かかか',
           // q3: 'ききき',
         }
